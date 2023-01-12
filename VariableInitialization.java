c65297a7b90d59ea8acff944f351f4278677a79b
ahleexxxx <122420468+ahleexxxx@users.noreply.github.com>
2023-01-12 16:53:21 -0600

    Add files via upload

---
+++ VariableInitialization.java
@@ -0,0 +1,14 @@
+package sec01.exam01;
+
+public class VariableInitialization {
+
+	public static void main(String[] args) {
+		int value=1;
+		
+		int result=value+10;
+		
+		System.out.println(result);
+
+	}
+
+}
